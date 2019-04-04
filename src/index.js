68271b0a30e5136e1058197c82cce005df2523da
Mickey <micksheridan.24@gmail.com>
2019-04-04 16:34:51 -0400

    submit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const taskForm = document.getElementById("create-task-form");
+  const taskInput = document.getElementById("new-task-description");
+  const tasks = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", function(e) {
+    const newTask = taskInput.value;
+    taskInput.value = "";
+    console.log(newTask);
+    const item = document.createElement("li");
+    item.textContent = newTask;
+    tasks.appendChild(item);
+
+    e.preventDefault();
+  });
 });
