9eed94595e4be8865d09f3a673d9160e4a9d40ec
stephenyoon84 <stephenyoon84@gmail.com>
2019-04-05 09:06:22 -0400

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form');
+
+  form.addEventListener('submit', addList);
+
+  function addList(event){
+    event.preventDefault();
+    const input = form.querySelector('#new-task-description').value;
+    form.querySelector('#new-task-description').value = "";
+    const li = document.getElementById('list').appendChild(document.createElement('li'));
+    li.innerText = input;
+    const btn = li.appendChild(document.createElement('button'));
+    btn.innerText = 'X';
+    btn.addEventListener('click', (e)=>{
+      // console.log(e.target);
+      li.remove();
+    })
+  }
 });
